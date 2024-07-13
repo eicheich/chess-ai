e9222022bc7cdbca7479bbabc1d03cffeb015d58
eicheich <nengindah41@gmail.com>
2024-07-13 13:49:59 +0700

    [HK] add surface color

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@ import pygame # type: ignore
 import sys
 
 from const import *
+from game import Game
 
 class Main:
     
@@ -9,9 +10,12 @@ class Main:
         pygame.init()
         self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
         pygame.display.set_caption("Chess")
+        self.game = Game()
     
     def mainloop(self):
         while True:
+            self.game.show_bg(self.screen)
+            
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
